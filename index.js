85e12bfc9899a14520eb6c702c3a6c77d9405a92
aleezamughal <aleezajaveedmughal@gmail.com>
2024-03-30 07:28:56 +0500

    programmed anumber guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// computer will generate random number
+// user input for guessing number
+// compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const guessNumber = await inquirer.prompt([{
+        name: "userguessedNumber",
+        type: "number",
+        message: "guess a number between 1-5:",
+    }]);
+if (guessNumber.userguessedNumber === randomNumber) {
+    console.log("welldone! your guess was accurate.");
+}
+else {
+    console.log("your guess was inaccurate");
+}
